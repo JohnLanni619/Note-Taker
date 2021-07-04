175142e1101153f75ca42c20e7879ee3b7f6756c
John Lanni <82123623+JohnLanni619@users.noreply.github.com>
2021-07-03 20:32:37 -0400

    Added api routes, create note function, errors

--- server.js
+++ server.js
@@ -1,16 +1,53 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
+const {notes} = require('./db/notes.json');
+
+const PORT = process.env.PORT || 3001;
 const app = express();
 
-const { notes } = require('./db/notes.json');
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/notes.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+}
 
-
+function validateNote(note) {
+    if (!note.title || typeof note.title !== 'string') {
+        return false;
+    }
+    if (!note.text || typeof note.text !== 'string') {
+        return false;
+    }
+    return true;
+}
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-  });
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    // if any data in req.body is inocorrect, send 400 error back
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    } 
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
+
+module.exports = notes;
